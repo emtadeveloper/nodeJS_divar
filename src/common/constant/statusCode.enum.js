83a08e44836b9bea540ce94465b3fd2548f2ae31
emtadeveloper <47769965+emtadeveloper@users.noreply.github.com>
2024-02-17 01:13:40 +0330

    -----

---
+++ src/common/constant/statusCode.enum.js
@@ -0,0 +1,10 @@
+const StatusCode = Object.freeze({
+    BadRequest: 400,
+    Unauthorized: 401,
+    Forbidden: 403,
+    NotFound: 404,
+    MethodNotAllowed: 405,
+    InternalServerError: 500,
+})
+
+module.exports = StatusCode
