b27a751d410626a521549c1c7fbf57028c772493
emtadeveloper <emta137671@gmail.com>
2024-02-17 16:32:26 +0330

    20-22

---
+++ src/modules/post/post.model.js
@@ -0,0 +1,13 @@
+const {Schema, Types, model} = require("mongoose")
+
+
+const PostSchema = new Schema({
+    title: {type: String, required: true},
+    content: {type: String, required: true},
+    category: {type: Types.ObjectId, ref: 'Category', required: true},
+    province: {type: String, required: true},
+    city: {type: String, required: true},
+    district: {type: String, required: true},
+    coordinate: {type: [Number], required: true}, // 52.44864866   25.54646546
+    images: {type: [String], required: false, default: []}
+})
